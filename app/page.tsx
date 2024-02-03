2cf5b4a17c330b990c3919c42ac577b0bf1d8e1e
backuardo <benjaminaeisner@gmail.com>
2024-02-03 08:45:28 -0500

    remove three

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import { NextPage } from "next";
 import { Flex, Heading } from "@radix-ui/themes";
 
-import { Scene } from "@/components/scene";
-
 const Home: NextPage = async () => {
 	return (
 		<Flex gap="4" direction="column" my="4" height="100%">
@@ -11,7 +9,6 @@ const Home: NextPage = async () => {
 					Index
 				</Heading>
 			</Flex>
-			<Scene />
 		</Flex>
 	);
 };
