c1f91513a0ddd398498bb185dd1357f40d1f7984
backuardo <benjaminaeisner@gmail.com>
2024-02-04 16:42:05 -0500

    remove tools page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,6 @@
 export const ROUTES = Object.freeze([
 	{ path: "/", name: "Index" },
 	{ path: "/about", name: "About" },
-	{ path: "/tools", name: "Tools" },
 	{ path: "/workbench", name: "Workbench" },
 ]);
 
