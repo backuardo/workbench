3f99e3e5c8565f96c8f2f4ca167e46912eba9e30
backuardo <benjaminaeisner@gmail.com>
2024-02-23 06:36:00 -0500

    fix link

--- components/ui/link.tsx
+++ components/ui/link.tsx
@@ -2,24 +2,26 @@ import InternalLink from "next/link";
 import { Link as ExternalLink } from "@radix-ui/themes";
 
 import { cn } from "@/lib/cn";
+import { ComponentProps } from "react";
 
 const externalLinkAttributes = Object.freeze({
 	target: "_blank",
 	rel: "noreferrer noopener",
 });
 
-export const Link: React.FC<{
-	href: string;
-	children: React.ReactNode | React.ReactNode[];
-	className?: string;
-}> = ({ href, children, className = "" }) => {
-	const isExternalUrl = href.startsWith("http");
+export const Link: React.FC<
+	ComponentProps<typeof ExternalLink | typeof InternalLink>
+> = ({ href = "", children, className = "", ...props }) => {
+	const isExternalUrl = typeof href === "string" && href.startsWith("http");
 	const _Link = isExternalUrl ? ExternalLink : InternalLink;
+	const linkProps = isExternalUrl
+		? { href, ...externalLinkAttributes }
+		: ({ href } as any);
 	return (
 		<_Link
-			href={href}
 			className={cn("text-gray-12 hover:text-accent-10", className)}
-			{...(isExternalUrl ? externalLinkAttributes : {})}
+			{...linkProps}
+			{...props}
 		>
 			{children}
 		</_Link>
